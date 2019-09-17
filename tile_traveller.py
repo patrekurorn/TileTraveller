e5eec5e541d364b4ff2bbdbe2690bcb60a2688ad
patrekurorn <patrekurf19@ru.is>
2019-09-17 09:58:07 +0000

    get_user_input funciton implemented

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,10 +1,16 @@
 # Gera starting position og geta hreyft sig í þær áttir sem hægt er
 
-def starting_posistion():
+#def starting_posistion():
 
 def get_user_input():
+    return input("Where do you want to move? ").lower()
 
-def what_direction_to_move():
 
-def is_user_user_in_range():
 
+#def what_direction_to_move():
+
+#def is_user_user_in_range():
+
+
+user_input = get_user_input()
+print("Direction: " + user_input)
