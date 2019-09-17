ba1147f83786ec918a56249e43e8c3f28f368366
Gretar Helgason <gretar19@ru.is>
2019-09-17 09:44:14 +0000

    the skeleton for the program

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,1 +1,10 @@
 # Gera starting position og geta hreyft sig í þær áttir sem hægt er
+
+def starting_posistion():
+
+def get_user_input():
+
+def what_direction_to_move():
+
+def is_user_user_in_range():
+
