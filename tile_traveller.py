0c6f60a8e660d3055906ae03814f41d608493f0e
patrekurorn <patrekurf19@ru.is>
2019-09-17 10:43:15 +0000

    working on def is_user_in_range

--- tile_traveller.py
+++ tile_traveller.py
@@ -23,7 +23,33 @@ def what_direction_to_move(current_user_input,x,y):
         x -=1
     return x, y
 
-#def is_user_user_in_range():
+def is_user_user_in_range(x, y):
+    # Here below is the whole frame
+    """if x > 3:
+        x = 3
+    if x < 1:
+        x = 1
+    if y > 3:
+        y = 3
+    if y < 1:
+        y = 1"""
+    # Here below is the allowed movements for the user
+    if x == 1 and y == 1:
+        print("You can travel: (N)orth.")
+        if x > 1:
+            x = 1
+            print("Not a valid direction!")
+        elif x < 1:
+            x = 1
+            print("Not a valid direction!")
+        elif y < 1:
+            y = 1
+            print("Not a valid direction!")
+        elif y > 1:
+            y += 1
+
+    
+    return x, y
 
 current_x, current_y = starting_posistion()
 user_input = get_user_input()
@@ -32,7 +58,7 @@ user_input = get_user_input()
 while user_input != "q":
 
     current_x, current_y = what_direction_to_move(user_input, current_x, current_y)
-
+    current_x, current_y = is_user_user_in_range(current_x, current_y)
 
     print("x", current_x)
     print("y", current_y)
